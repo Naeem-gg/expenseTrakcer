3fbdfa8883b75bae2819c609015d88d82a954d8e
naeem <naeemqg1@gmail.com>
2022-11-07 21:10:09 +0530

    added popup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended:true }));
+
+app.get('/', (req, res) => {
+res.sendFile(__dirname + '/public/popup.html');
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
+
+app.listen(port, () => console.log(`Server listening on port ${port}!\nhttp://localhost:3000`));
