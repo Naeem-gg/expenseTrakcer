12878b3bb9c89e74c4bba62b73a1bbcfd1d0c331
naeem <naeemqg1@gmail.com>
2022-11-07 23:45:55 +0530

    added crud details

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended:true }));
 
 app.get('/', (req, res) => {
-res.sendFile(__dirname + '/public/popup.html');
+res.sendFile(__dirname + '/public/index.html');
 });
 
 app.post('/', (req, res) => {
     console.log(req.body);
-    res.send(req.body);
+    res.send(`<h1>req.body</h1>`);
 });
 
 app.listen(port, () => console.log(`Server listening on port ${port}!\nhttp://localhost:3000`));
